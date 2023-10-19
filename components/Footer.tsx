04669c57f8801dad784efad768ee07d4e484d304
dnwls7738 <cjsdnwls7738@gmail.com>
2023-10-19 17:53:04 +0900

    hist 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,10 +35,15 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      
+      <div className={styles.hits}>              
+        <a href="https://hits.seeyoufarm.com">
+          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fwoojin.vercel.app&count_bg=%2303C75A&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=visits&edge_flat=false"/>
+        </a>
         <div className={styles.copyright}>
           Copyright 2022 {config.author}
         </div> 
+      </div>
+
 
       <div className={styles.settings}>
         {hasMounted && (
