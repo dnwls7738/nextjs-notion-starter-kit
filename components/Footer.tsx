0a4d3bc6625888955fdbf5c3e1d5cb599343bb1e
dnwls7738 <cjsdnwls7738@gmail.com>
2023-10-19 14:08:55 +0900

    footer 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -135,8 +135,8 @@ export const FooterImpl: React.FC = () => {
           </a>
         )}
       </div>
-      <a href="https://hits.seeyoufarm.com">
-        <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter"/>
+      <a href="https://woojin.vercel.app/">
+        <img src="https://woojin.vercel.app/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter"/>
       </a>                        
     </footer>
   )
