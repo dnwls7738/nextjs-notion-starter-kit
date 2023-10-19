1ea6939adce84c08ba79c2c879ffeb1fccb9ae9a
dnwls7738 <cjsdnwls7738@gmail.com>
2023-10-19 14:32:28 +0900

    조회수 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,12 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div> 
+      <div className={styles.copyright}>
+        Copyright 2022 {config.author}
+        <a href="https://hits.seeyoufarm.com">
+          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fwoojin.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/>
+        </a>  
+      </div> 
 
       <div className={styles.settings}>
         {hasMounted && (
