9b2f276e3d4da1d841b38fb81d9f2e2e19c101d3
dnwls7738 <cjsdnwls7738@gmail.com>
2023-10-20 11:51:45 +0900

    채널톡 설치

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -67,8 +67,46 @@ export default class MyDocument extends Document {
     localStorage.setItem(storageKey, JSON.stringify(isDarkMode))
   }
 })();
+(function() {
+  var w = window;
+  if (w.ChannelIO) {
+    return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+  }
+  var ch = function() {
+    ch.c(arguments);
+  };
+  ch.q = [];
+  ch.c = function(args) {
+    ch.q.push(args);
+  };
+  w.ChannelIO = ch;
+  function l() {
+    if (w.ChannelIOInitialized) {
+      return;
+    }
+    w.ChannelIOInitialized = true;
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+    s.charset = 'UTF-8';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+  }
+  if (document.readyState === 'complete') {
+    l();
+  } else if (window.attachEvent) {
+    window.attachEvent('onload', l);
+  } else {
+    window.addEventListener('DOMContentLoaded', l, false);
+    window.addEventListener('load', l, false);
+  }
+})();
+ChannelIO('boot', {
+  "pluginKey": "21c31f7f-5312-4466-bb81-85bf62d8396f"
+});
 `
-              }}
+              }}              
             />
             <Main />
 
