87602510a9e746043b1c54aa666b1332f38bdaf6
dnwls7738 <cjsdnwls7738@gmail.com>
2023-10-20 12:06:35 +0900

    플러그인키 변수 설정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -105,7 +105,7 @@ export default class MyDocument extends Document {
   }
 })();
 ChannelIO('boot', {
-  "pluginKey": process.env.CHANNAL_TALK
+  "pluginKey": "${process.env.NEXT_PUBLIC_CHANNAL_TALK}"
 });
 `
               }}              
